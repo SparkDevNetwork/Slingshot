e93bca8ae1734e2e83ff41d22fcf6f84f9f01d54
Mike Peterson <mike@sparkabilitygroup.com>
2017-08-15 09:31:00 -0700

    - Fix thread exception when setting the DumpResponseToXmlFile

--- Slingshot.CCB/Slingshot.CCB/MainWindow.xaml.cs
+++ Slingshot.CCB/Slingshot.CCB/MainWindow.xaml.cs
@@ -37,6 +37,9 @@ namespace Slingshot.CCB
 
             _apiUpdateTimer.Tick += _apiUpdateTimer_Tick; ;
             _apiUpdateTimer.Interval = new TimeSpan( 0, 2, 30 );
+            
+            // Set CcbApi.DumpResponseToXmlFile to true to save all API Responses to XML files and include them in the slingshot package
+            CcbApi.DumpResponseToXmlFile = cbDumpResponseToXmlFile.IsChecked ?? false;
 
             exportWorker.DoWork += ExportWorker_DoWork;
             exportWorker.RunWorkerCompleted += ExportWorker_RunWorkerCompleted;
@@ -66,8 +69,7 @@ namespace Slingshot.CCB
             // clear filesystem directories
             CcbApi.InitializeExport();
 
-            // Set CcbApi.DumpResponseToXmlFile to true to save all API Responses to XML files and include them in the slingshot package
-            CcbApi.DumpResponseToXmlFile = cbDumpResponseToXmlFile.IsChecked ?? false;
+            
 
             // export individuals
             if ( exportSettings.ExportIndividuals )
