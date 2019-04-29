ce8d67646ce6499b08662f5dee8c55949a9cef8c
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2019-04-29 15:12:50 -0400

    Add Business Address and Phone to the export.

---
+++ Slingshot.F1/Slingshot.F1/Utilities/Translators/MDB/F1BusinessAddress.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+
+using Slingshot.Core;
+using Slingshot.Core.Model;
+
+namespace Slingshot.F1.Utilities.Translators.MDB
+{
+    public static class F1BusinessAddress
+    {
+        public static BusinessAddress Translate( DataRow row )
+        {
+
+            var address = new BusinessAddress();
+
+            try
+            {
+                address.BusinessId = row.Field<int>( "HOUSEHOLD_ID" ) + +900000000;
+                address.Street1 = row.Field<string>( "address_1" );
+                address.Street2 = row.Field<string>( "address_2" );
+                address.City = row.Field<string>( "city" );
+                address.State = row.Field<string>( "state" );
+                address.PostalCode = row.Field<string>( "zip_code" );
+                address.Country = row.Field<string>( "country" );
+
+                var addressType = row.Field<string>( "address_type" );
+                switch ( addressType )
+                {
+                    case "Primary":
+                    {
+                        address.AddressType = AddressType.Home;
+                        address.IsMailing = true;
+                        break;
+                    }
+                    case "Previous":
+                    {
+                        address.AddressType = AddressType.Previous;
+                        break;
+                    }
+                    case "Business":
+                    {
+                        address.AddressType = AddressType.Work;
+                        break;
+                    }
+                    case "Mail Returned / Incorrect":
+                    {
+                        address.AddressType = AddressType.Other;
+                        break;
+                    }
+                    default:
+                    {
+                        address.AddressType = AddressType.Other;
+                        break;
+                    }
+                }
+
+                // only add the address if we have a valid address
+                if ( address.Street1.IsNotNullOrWhitespace() &&
+                        address.City.IsNotNullOrWhitespace() &&
+                        address.PostalCode.IsNotNullOrWhitespace() )
+                {
+                    return address;
+
+                }
+
+                return null;
+
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ Slingshot.F1/Slingshot.F1/Utilities/Translators/MDB/F1BusinessPhone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+
+using Slingshot.Core;
+using Slingshot.Core.Model;
+
+namespace Slingshot.F1.Utilities.Translators.MDB
+{
+    public static class F1BusinessPhone
+    {
+        public static BusinessPhone Translate( DataRow row )
+        {
+            var phone = new BusinessPhone();
+
+            try
+            {
+                string phoneType = row.Field<string>( "communication_type" ).Replace( "Phone", "" ).Replace( "phone", "" ).Trim();
+                string phoneNumber = new string( row.Field<string>( "communication_value" ).Where( c => char.IsDigit( c ) ).ToArray() );
+                if ( !string.IsNullOrWhiteSpace( phoneNumber ) )
+                {
+                    phone.BusinessId = row.Field<int>( "HOUSEHOLD_ID" ) + +900000000;
+                    phone.PhoneType = phoneType;
+                    phone.PhoneNumber = phoneNumber.Left( 20 );
+                    return phone;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
