2bce7f040c690d8afe6be550626a3f59d82dba95
Shaun Cummings <shaun@sparkabilitygroup.com>
2022-04-25 15:10:22 -0600

    - Update some Rock dependencies to use project references instead of direct file links, updated projects to .NET 4.7.2.

--- Slingshot/Properties/Resources.Designer.cs
+++ Slingshot/Properties/Resources.Designer.cs
@@ -10,7 +10,7 @@
 
 namespace Slingshot.Properties
 {
-
+    using System;
 
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -19,14 +19,12 @@ namespace Slingshot.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute( "System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0" )]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute( "System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0" )]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources
     {
-
         private static global::System.Resources.ResourceManager resourceMan;
-
         private static global::System.Globalization.CultureInfo resourceCulture;
 
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute( "Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode" )]
@@ -42,7 +40,7 @@ namespace Slingshot.Properties
         {
             get
             {
-                if ( ( resourceMan == null ) )
+                if ( object.ReferenceEquals( resourceMan, null ) )
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager( "Slingshot.Properties.Resources", typeof( Resources ).Assembly );
                     resourceMan = temp;
